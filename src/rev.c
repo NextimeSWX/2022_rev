c351d4d31feb82d5d25d6f7b8c2dea34b01167cd
Nextime <evan.lebihan@ecole-89.com>
2022-12-06 15:27:06 +0100

    Rev

--- src/rev.c
+++ src/rev.c
@@ -9,26 +9,17 @@
 #include <stdlib.h>
 #include "stu.h"
 
-
-
-#include <stdio.h>
-
-
-void put_rev(int nb, int size_read, char *str, char *buffer)
+void put_rev(int nb, char *str, char *buffer)
 {
     int i;
-    int j;
 
     i = nb;
-    j = 0;
-    while(j != size_read) {
-        while(buffer[i] != '\n' && i >= 0) {
-            str = &buffer[i];
-            write(1, str, 1);
-            i -= 1;
-        }
-    stu_putchar('\n');
+    while(buffer[i] != '\n' && i >= 0) {
+        str = &buffer[i];
+        write(1, str, 1);
+        i -= 1;
     }
+    stu_putchar('\n');
 }
 
 int rev(int size_read, char *buffer)
@@ -37,19 +28,13 @@ int rev(int size_read, char *buffer)
     int nb;
 
     str = 0;
-    nb = 0;/*
-             if (size_read != '\n') {
-             nb = size_read;
-             buffer[size_read] = '\0';
-             put_rev(nb, str, buffer);
-             } else {*/
-    while(size_read >= nb) {
+    nb = 0;
+    while(size_read > nb) {
         while(buffer[nb] != '\n' && buffer[nb] != '\0') {
             nb += 1;
         }
         nb -= 1;
-        put_rev(nb, size_read, str, buffer);
-        //}
+        put_rev(nb, str, buffer);
         nb += 2;
     }
     return(0);
