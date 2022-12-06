e3b20e8bf6409f4636d6a824ca556dacce296609
Nextime <evan.lebihan@ecole-89.com>
2022-12-06 15:56:29 +0100

    Rev

---
+++ include/stu.h
@@ -0,0 +1,16 @@
+/*
+ * E89 Pedagogical & Technical Lab
+ * project:     Rev
+ * created on:  2022-11-22 - 09:48 +0100
+ * 1st author:  evan.lebihan - evan.lebihan
+ * description: stu.h
+ */
+
+#ifndef STU_H_
+#define STU_H_
+
+int rev(int size_read, char *buffer);
+void stu_putchar(char c);
+void put_rev(int nb, char *str, char *buffer);
+
+#endif
